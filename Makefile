e7c1cabc838d6c4273b949033c931542d97ab177
Ubuntu <ubuntu@ip-172-31-47-61.ap-south-1.compute.internal>
2022-06-28 08:33:50 +0000

    add a message

---
+++ Makefile
@@ -0,0 +1,6 @@
+abcd.exe: main.o add.o
+	gcc -o abcd.exe main.o add.o
+main.o: main.c
+	gcc -c main.c
+add.o: add.c
+	gcc -c add.c
---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void add()
+{
+	int a,b,c ;
+	printf(" enter two numbers\n");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("sum=%d\n", c);
+}
+
